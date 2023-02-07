c5de255dd92181a1ffde6b3f02388bac87dabd4c
cwenhe <wenhe.chen@youhualin.com>
2023-02-07 16:16:01 +0800

    chg:更新配置

--- config.lua
+++ config.lua
@@ -53,6 +53,7 @@ lvim.builtin.which_key.mappings["c"] = {
     C = { "<cmd> Task start cmake clean<cr>", "clean" },
     e = { "<cmd> FencView<cr>", "change file encoding" }
 }
+lvim.builtin.which_key.mappings["sq"] = { "<cmd> cope<CR>", "show quickfix" }
 
 lvim.builtin.which_key.mappings["w"] = {
     name = "Windows",
@@ -201,7 +202,7 @@ require('tasks').setup({
     default_params = { -- Default module parameters with which `neovim.json` will be created.
         cmake = {
             cmd = 'cmake', -- CMake executable to use, can be changed using `:Task set_module_param cmake cmd`.
-            build_dir = tostring(Path:new('{cwd}', 'build', '{os}-{build_type}')), -- Build directory. The expressions `{cwd}`, `{os}` and `{build_type}` will be expanded with the corresponding text values. Could be a function that return the path to the build directory.
+            build_dir = tostring(Path:new('{cwd}', 'build')), --, '{os}-{build_type}')), -- Build directory. The expressions `{cwd}`, `{os}` and `{build_type}` will be expanded with the corresponding text values. Could be a function that return the path to the build directory.
             build_type = 'Debug', -- Build type, can be changed using `:Task set_module_param cmake build_type`.
             dap_name = 'lldb', -- DAP configuration name from `require('dap').configurations`. If there is no such configuration, a new one with this name as `type` will be created.
             args = { -- Task default arguments.
