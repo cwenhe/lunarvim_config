f68d3233b007ccaa78164c397eefe67f55292844
cwenhe <wenhe.chen@youhualin.com>
2023-03-20 10:31:23 +0800

    chg:修改配置

--- config.lua
+++ config.lua
@@ -17,6 +17,7 @@ vim.opt.expandtab = true
 
 -- general
 lvim.log.level = "info"
+lvim.lsp.log_levels = "OFF"
 lvim.format_on_save = {
     enabled = true,
     pattern = "*.lua",
@@ -44,6 +45,7 @@ lvim.keys.insert_mode["jk"] = "<esc>"
 -- -- Use which-key to add extra bindings with the leader-key prefix
 lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 lvim.builtin.which_key.mappings["bw"] = { "<cmd>noautocmd w<cr>", "Save without formatting" }
+lvim.builtin.which_key.mappings["bc"] = { "<cmd>enew<cr>", "new buffer" }
 
 lvim.builtin.which_key.mappings["x"] = { "<cmd> bdelete!<CR>", "close buffer" }
 lvim.builtin.which_key.mappings["c"] = {
@@ -75,7 +77,7 @@ lvim.builtin.which_key.mappings["w-"] = { "<cmd> exec 'vertical resize -'. strin
     "windows resize -" }
 
 -- -- Change theme settings
-lvim.colorscheme = "catppuccin_latte"
+lvim.colorscheme = "tokyonight-day"
 
 lvim.builtin.lualine.style = "default"
 lvim.builtin.alpha.active = true
@@ -148,17 +150,24 @@ lvim.plugins = {
     --   "folke/trouble.nvim",
     --   cmd = "TroubleToggle",
     -- },
+    -- {"weilbith/nvim-code-action-menu"},
+    {
+        "ianding1/leetcode.vim"
+    },
+    { "EdenEast/nightfox.nvim"},
     { 'Shatur/neovim-tasks' },
     { 'mbbill/fencview' },
     { 'danymat/neogen',
         config = function()
-            require('neogen').setup {}
+            require('neogen').setup({ snippet_engine = "luasnip" })
         end },
     { "azabiong/vim-highlighter" },
     { "ellisonleao/gruvbox.nvim" },
     { "folke/tokyonight.nvim" },
+    {"EdenEast/nightfox.nvim"},
     { "Mofiqul/vscode.nvim" },
     { "catppuccin/vim" },
+    -- {"p00f/clangd_extensions.nvim"},
     --clipboard
     { "ojroques/nvim-osc52" },
     {
@@ -179,6 +188,9 @@ lvim.plugins = {
             require("todo-comments").setup()
         end,
     },
+    {
+        "rcarriga/nvim-dap-ui"
+    }
     -- {
     --     "zbirenbaum/copilot.lua",
     --     cmd = "Copilot",
@@ -238,6 +250,7 @@ require('tasks').setup({
 --     },
 -- }
 
+-- require'lspconfig'.cmake.setup{}
 
 lvim.builtin.treesitter.highlight.disable = function(lang, buf)
     if lang == "json" then
@@ -278,6 +291,7 @@ lvim.builtin.cmp.formatting = {
 --     enabled = true
 -- }
 lvim.builtin.luasnip.sources.friendly_snippets = false
+-- lvim.builtin.luasnip.sources.friendly_snippets = false
 lvim.builtin.indentlines.options.show_trailing_blankline_indent = true
 lvim.builtin.indentlines.options.show_current_context = true
 lvim.builtin.telescope.defaults.path_display = {
@@ -287,7 +301,7 @@ lvim.builtin.telescope.defaults.path_display = {
 }
 lvim.builtin.telescope.defaults.file_ignore_patterns = { "^./.git/", "^./build/", "^.vscode/", "%%-test%-data",
     "^.cache/" }
-require("dap.ext.vscode").load_launchjs(nil, { lldb = { "c", "cpp" } })
+lvim.builtin.dap.active = true
 
 
 -----------------------begin clipboard config -------------------------------------------
@@ -314,3 +328,135 @@ vim.keymap.set('n', '<leader>cc', '"+yy')
 vim.keymap.set('n', '<leader>c', '"+y')
 vim.keymap.set('n', '<leader>cc', '"+yy')
 -----------------------end clipboard config -------------------------------------------
+--
+--
+----------------------- begin clangd_extensions --------------------------------------
+-- require("clangd_extensions").setup()
+-----------------------  end clangd_extensions  --------------------------------------
+--
+-----------------------  begin dap config --------------------------------------
+local dap = require('dap')
+dap.adapters.cppdbg = {
+  id = "cppdbg",
+  type = "executable",
+  -- command = "/usr/bin/lldb-vscode-11",
+  -- command = "/home/cwenhe/3rd_softwares/extension/debugAdapters/bin/OpenDebugAD7",
+  command = "OpenDebugAD7",
+  name = "lldb",
+  env = {
+    LLDB_LAUNCH_FLAG_LAUNCH_IN_TTY = "NO",
+  },
+}
+
+dap.adapters.lldb = {
+  type = 'executable',
+  command = 'lldb-vscode', -- adjust as needed, must be absolute path
+  name = 'lldb'
+}
+
+require("dap.ext.vscode").load_launchjs(nil, { 
+    cppdbg= { "c", "cpp" } ,
+    lldb = { "c", "cpp" } 
+})
+-----------------------  end dap config --------------------------------------
+
+
+-----------------------  begin dap ui config --------------------------------------
+local dap, dapui = require "dap", require "dapui"
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dapui.close()
+  dap.repl.close {}
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dapui.close()
+  dap.repl.close {}
+end
+
+dapui.setup {
+  icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
+  mappings = {
+    -- Use a table to apply multiple mappings
+    expand = { "<CR>", "<2-LeftMouse>", "l" },
+    open = "o",
+    remove = "d",
+    edit = "e",
+    repl = "r",
+    toggle = "t",
+  },
+  -- Expand lines larger than the window
+  -- Requires >= 0.7
+  expand_lines = vim.fn.has "nvim-0.7" == 1,
+  -- Layouts define sections of the screen to place windows.
+  -- The position can be "left", "right", "top" or "bottom".
+  -- The size specifies the height/width depending on position. It can be an Int
+  -- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
+  -- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
+  -- Elements are the elements shown in the layout (in order).
+  -- Layouts are opened in order so that earlier layouts take priority in window sizing.
+  layouts = {
+    -- {
+    --   elements = {
+    --     -- Elements can be strings or table with id and size keys.
+    --     { id = "scopes", size = 0.25 },
+    --     "breakpoints",
+    --     "stacks",
+    --     "watches",
+    --   },
+    --   size = 20, -- 40 columns
+    --   position = "left",
+    -- },
+    {
+      elements = {
+        "repl",
+        "stacks",
+        -- "console",
+      },
+      size = 0.25, -- 25% of total lines
+      position = "bottom",
+    },
+  },
+  controls = {
+    -- Requires Neovim nightly (or 0.8 when released)
+    enabled = true,
+    -- Display controls in this element
+    element = "repl",
+    icons = {
+      pause = "",
+      play = "",
+      step_into = "",
+      step_over = "",
+      step_out = "",
+      step_back = "",
+      run_last = "↻",
+      terminate = "□",
+    },
+  },
+  floating = {
+    max_height = nil, -- These can be integers or a float between 0 and 1.
+    max_width = nil, -- Floats will be treated as percentage of your screen.
+    border = "single", -- Border style. Can be "single", "double" or "rounded"
+    mappings = {
+      close = { "q", "<Esc>" },
+    },
+  },
+  windows = { indent = 1 },
+  render = {
+    max_type_length = nil, -- Can be integer or nil.
+    max_value_lines = 100, -- Can be integer or nil.
+  },
+}
+
+lvim.keys.normal_mode["<F5>"] = " <Cmd>lua require'dap'.continue()<CR>"
+lvim.keys.normal_mode["<F10>"] = " <Cmd>lua require'dap'.step_over()<CR>"
+lvim.keys.normal_mode["<F11>"] = " <Cmd>lua require'dap'.step_into()<CR>"
+lvim.keys.normal_mode["<S-F10>"] = " <Cmd>lua require'dap'.step_out()<CR>"
+lvim.keys.normal_mode["<F12>"] = " <Cmd>lua require'dap'.step_out()<CR>"
+lvim.keys.normal_mode["<S-f5>"] = " <Cmd>lua require'dap'.continue()<CR>"
+-- lvim.keys.normal_mode["<Leader>ds"] = { " <Cmd>lua require'dap'.terminate()<CR>", "terminate debug" },
+lvim.keys.normal_mode["<F9>"] =  " <Cmd>lua require'dap'.toggle_breakpoint()<CR>"
+
+
+-----------------------  end dap ui config --------------------------------------
