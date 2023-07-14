6c6c80e7c08ab5294dfd00a9c38056381d634f46
cwenhe <wenhe.chen@youhualin.com>
2023-07-14 13:49:03 +0800

    chg:修改配置

--- config/telescope.lua
+++ config/telescope.lua
@@ -9,6 +9,31 @@ lvim.builtin.telescope.defaults.path_display = {
 }
 
 
+-- lvim.builtin.telescope.defaults.mappings.n["ww"] = function(prompt_bufnr)
+--     -- Use nvim-window-picker to choose the window by dynamically attaching a function
+--     local action_set = require('telescope.actions.set')
+--     local action_state = require('telescope.actions.state')
+
+--     local picker = action_state.get_current_picker(prompt_bufnr)
+--     picker.get_selection_window = function(picker, entry)
+--         local picked_window_id = require('window-picker').pick_window() or vim.api.nvim_get_current_win()
+--         -- Unbind after using so next instance of the picker acts normally
+--         picker.get_selection_window = nil
+--         return picked_window_id
+--     end
+
+--     return action_set.edit(prompt_bufnr, 'edit')
+-- end
+
+
+
+-- function(prompt_bufnr)
+--     actions.select_horizontal({ prompt_bufnr })
+--     --     local selection = require("telescope.actions.state").get_selected_entry()
+--     -- local dir = vim.fn.fnamemodify(selection.path, ":p:h")
+--     -- require("telescope.actions").close(prompt_bufnr)
+--     -- -- Depending on what you want put `cd`, `lcd`, `tcd`
+--     -- vim.cmd(string.format("silent lcd %s", dir))
+-- end
 -- local action_layout = require("telescope.actions.layout")
--- lvim.builtin.telescope.defaults.mappings.n["<M-p>"] = action_layout.toggle_preview
 -- lvim.builtin.telescope.defaults.mappings.i["<M-p>"] = action_layout.toggle_preview
