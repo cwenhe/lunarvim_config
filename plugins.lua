a933576c1c326569943291b0bfbf2c21d1eab939
cwenhe <wenhe.chen@youhualin.com>
2023-08-28 11:21:59 +0800

    chg:删除无用插件

--- plugins.lua
+++ plugins.lua
@@ -128,10 +128,10 @@ lvim.plugins = {
     --         require('config.nvim-bqf')
     --     end,
     -- },
-    {
-        "ray-x/lsp_signature.nvim",
-        event = "VeryLazy",
-        opts = {},
-        config = function(_, opts) require 'lsp_signature'.setup(opts) end
-    }
+    -- {
+    --     "ray-x/lsp_signature.nvim",
+    --     event = "VeryLazy",
+    --     opts = {},
+    --     config = function(_, opts) require 'lsp_signature'.setup(opts) end
+    -- }
 }
