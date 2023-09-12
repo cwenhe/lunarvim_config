f3e0385aeccda6aadbd820845a6f47fee97aeac3
cwenhe <wenhe.chen@youhualin.com>
2023-09-12 14:26:32 +0800

    chg:更新配置

--- config/nvimtree.lua
+++ config/nvimtree.lua
@@ -1,9 +1,9 @@
 local api = require('nvim-tree.api')
 
-lvim.builtin.nvimtree.setup.view.mappings = {
-    list = {
-        {
-            key = "v", action = api.node.open.vertical, mode = 'n'
-        }
-    }
-}
+-- lvim.builtin.nvimtree.setup.view.mappings = {
+--     list = {
+--         {
+--             key = "v", action = api.node.open.vertical, mode = 'n'
+--         }
+--     }
+-- }
