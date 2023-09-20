9b58e7c9eadcf601ff8192165dc533e27f129337
cwenhe <wenhe.chen@youhualin.com>
2023-09-20 16:08:10 +0800

    chg:修改大文件检测

--- config/bigfile.lua
+++ config/bigfile.lua
@@ -1,13 +1,38 @@
-require("bigfile").config {
-    filesize = 1,      -- size of the file in MiB, the plugin round file sizes to the closest MiB
-    pattern = { "*" }, -- autocmd pattern
-    features = {       -- features to disable
-        "indent_blankline",
-        "illuminate",
-        "treesitter",
-        "syntax",
-        "matchparen",
-        "vimopts",
-        "filetype",
-    },
-}
+-- require("bigfile").config {
+--     filesize = 1, -- size of the file in MiB, the plugin round file sizes to the closest MiB
+--     -- pattern = { "*" }, -- autocmd pattern
+--     pattern = function(bufnr, filesize_mib)
+--         -- you can't use `nvim_buf_line_count` because this runs on BufReadPre
+--         -- local file_contents = vim.fn.readfile(vim.api.nvim_buf_get_name(bufnr))
+--         -- local file_length = #file_contents
+--         local filetype = vim.filetype.match({ buf = bufnr })
+--         local file_size = vim.fn.getfsize(vim.api.nvim_buf_get_name(bufnr))
+--         print(file_size)
+--         print(file_size)
+--         print(file_size)
+--         print(file_size)
+--         print(file_size)
+--         if file_size > 1024 * 10 and filetype == "json" then
+--             return true
+--         end
+--     end,
+--     features = { -- features to disable
+--         "indent_blankline",
+--         "illuminate",
+--         "treesitter",
+--         "syntax",
+--         "matchparen",
+--         "vimopts",
+--         "filetype",
+--     },
+-- }
+lvim.builtin.bigfile.config.pattern = function(bufnr, filesize_mib)
+    -- you can't use `nvim_buf_line_count` because this runs on BufReadPre
+    -- local file_contents = vim.fn.readfile(vim.api.nvim_buf_get_name(bufnr))
+    -- local file_length = #file_contents
+    local filetype = vim.filetype.match({ buf = bufnr })
+    local file_size = vim.fn.getfsize(vim.api.nvim_buf_get_name(bufnr))
+    if file_size > 1024 * 10 and filetype == "json" then
+        return true
+    end
+end
